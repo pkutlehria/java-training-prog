aa97f3d62eb02ad914029dbb969f037f6cb7305b
Prerna Kutlehria <prernakutlehria2003@gmail.com>
2023-06-12 12:15:18 +0530

    edited

--- programs/Program12.java
+++ programs/Program12.java
@@ -6,16 +6,20 @@ public class Program12{
 				Scanner s = new Scanner(System.in);
 				int size = s.nextInt();
 				arr = new int[size];
+				int sum = 0;
 				
 				for(int i = 0; i < size ; i++){
 					System.out.println("Enter value at position "+(i+1));
 					arr[i] = s.nextInt();
-				}
+					sum = sum + arr[i];
+					}
 				
 				System.out.println("Displaying values::");
 				for(int i = 0; i < size ; i++){
 					System.out.println("Value at "+(i+1)+"  position is "+ arr[i]);
 					
 				}
+				
+				System.out.println("The sum the values is ::"+sum);
 	}
 }
