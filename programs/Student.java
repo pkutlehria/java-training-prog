1ddd40cb84be63225f7daef2d522f2dcd6371589
Prerna Kutlehria <prernakutlehria2003@gmail.com>
2023-06-15 10:00:50 +0530

    constructor

---
+++ programs/Student.java
@@ -0,0 +1,21 @@
+//Write a program to print the names of students by creating a Student class.
+ //If no name is passed while creating an object of Student class,
+ //then the name should be "Unknown", otherwise the name should be equal to the String value
+ //passed while creating object of Student class
+ 
+ import java.util.Scanner;
+ public class Student{
+	 Student(){
+		 System.out.println("Unknown");
+		 
+	 }
+	 
+	 Student(String s){
+		 System.out.println(s);
+	 }
+	 public static void main(String args[]){
+		System.out.println("Enter the name of the student");
+		Student s = new Student();
+		Student s1 = new Student("Prerna");
+	 }
+ }
