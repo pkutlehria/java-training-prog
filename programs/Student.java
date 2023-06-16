086e6668dd13dce33c2dc3778967855a530a78f6
Prerna Kutlehria <prernakutlehria2003@gmail.com>
2023-06-16 11:18:08 +0530

    Updated Student.java

--- programs/Student.java
+++ programs/Student.java
@@ -19,19 +19,3 @@
 	 }
  }
  
- public class Program14{
-	int a =0;
-	static int b = 0;
-	public void increment(){
-		a++;
-		b++;
-		System.out.println("a="+a+",b="+b);
-	}
-	public static void main(String args[]){
-		Program14 obj = new Program14();
-		obj.increment();               //a=1,b=2
-		
-		Program14 obj2 = new Program14();
-		obj2.increment();             //a=1,b=2
-	}
-}
